9be22c0b05a5e196b6aca3ee1bd3d1509bace38f
Michał Poręba <michalporeba@gmail.com>
2022-01-06 17:25:00 +0000

    experiments with flutter widget package development

---
+++ fuxim_demo/android/app/src/main/kotlin/com/example/fuxim_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fuxim_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ fuxim_demo/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:fuxim/fuxim.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            MyWidget(title: 'T', message: 'M'),
+          ],
+        ),
+      ),
+    );
+  }
+}
