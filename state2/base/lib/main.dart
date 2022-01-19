be9dd82d8a6e2af386209fc3f7e122e8e22e8f3c
Michał Poręba <michalporeba@gmail.com>
2022-01-19 15:32:22 +0000

    startup project for state demo

---
+++ state2/base/lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'chart.dart';
+import 'slider.dart';
+
+void main() {
+  runApp(const SampleStateApp());
+}
+
+class SampleStateApp extends StatelessWidget {
+  const SampleStateApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Experiments with state',
+      home: Home()
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('managing state sample')),
+        body: Center(
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(32.0),
+                child: Text('00:00',
+                style: TextStyle(fontSize: 32, color: Colors.blueGrey)),
+              ),
+              const Spacer(),
+              Padding(
+                padding: const EdgeInsets.all(32),
+                child: SizedBox(
+                    width:50, height:50,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor
+                    ),
+                  )
+                ),
+              ),
+              const Spacer(),
+              Padding(
+                padding: const EdgeInsets.all(32),
+                child: Column(
+                  children: [
+                    const MySlider(),
+                    const MySlider(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children:const [
+                        ElevatedButton(onPressed: null, child: Text('red')),
+                        ElevatedButton(onPressed: null, child: Text('green')),
+                        ElevatedButton(onPressed: null, child: Text('blue'))
+                      ]
+                    )
+                  ]
+                )
+              ),
+              const Padding(
+                  padding: EdgeInsets.all(32),
+
+              )
+            ]
+          )
+        )
+    );
+  }
+}
