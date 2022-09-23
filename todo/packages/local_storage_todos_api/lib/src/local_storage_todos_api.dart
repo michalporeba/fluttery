ded78d6a6dc525608f9537dfe67cc9c71f1dde3a
Michał Poręba <michalporeba@gmail.com>
2022-09-23 23:01:31 +0100

    adding packages to the todo app

---
+++ todo/packages/local_storage_todos_api/lib/src/local_storage_todos_api.dart
@@ -0,0 +1,14 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+/// {@template local_storage_todos_api}
+/// A Flutter implementation of the TodosApi that uses local storage.
+/// {@endtemplate}
+class LocalStorageTodosApi {
+  /// {@macro local_storage_todos_api}
+  const LocalStorageTodosApi();
+}
---
+++ todo/packages/todos_api/lib/src/todos_api.dart
@@ -0,0 +1,14 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+/// {@template todos_api}
+/// The interface and models for an API providing access to todos.
+/// {@endtemplate}
+class TodosApi {
+  /// {@macro todos_api}
+  const TodosApi();
+}
